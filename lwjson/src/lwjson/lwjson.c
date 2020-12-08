19d04b57705510743ffa9546030e0dbd1812c494
Tilen Majerle <tilen.majerle@gmail.com>
2020-12-08 21:02:33 +0100

    Set version to 1.0.2

--- lwjson/src/lwjson/lwjson.c
+++ lwjson/src/lwjson/lwjson.c
@@ -29,7 +29,7 @@
  * This file is part of LwJSON - Lightweight JSON format parser.
  *
  * Author:          Tilen MAJERLE <tilen@majerle.eu>
- * Version:         v1.0.1
+ * Version:         v1.0.2
  */
 #include <string.h>
 #include "lwjson/lwjson.h"
